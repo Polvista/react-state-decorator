aead66fd189404d3c845d50c1293c23670908e56
Alexey Sokolov <sokol789@yandex.ru>
2017-02-06 16:50:36 +0300

    optimization

--- tests/integration/App.jsx
+++ tests/integration/App.jsx
@@ -5,11 +5,18 @@ export default class App extends Component {
 
     @track count = 0;
     @track title = 'Hello';
+    @track task = {
+        id: 1
+    };
 
     increment() {
         this.count++;
         this.count++;
     }
+
+    changeTask = () => {
+        this.task = { id: 2 };
+    };
 
     render() {
         console.log('render');
@@ -19,8 +26,11 @@ export default class App extends Component {
                 <br />
                 {this.count}
                 <br />
+                Task id: {this.task.id}
+                <br />
                 <button onClick={() => this.increment()}>Increment</button>
                 <button onClick={() => this.title = 'Hello there'}>Change title</button>
+                <button onClick={this.changeTask}>Change task id</button>
             </div>
         );
     }
