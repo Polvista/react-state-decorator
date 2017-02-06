598378083ceaa945e17b23baeb911143b0bee12c
Alexey Sokolov <sokol789@yandex.ru>
2017-02-06 15:57:38 +0300

    simple track realization

--- src/track.js
+++ src/track.js
@@ -1,3 +1,19 @@
-export function track() {
-    console.log('hey');
+export function track(target, propertyName, descriptor) {
+    let value;
+
+    if(descriptor.initializer) {
+        value = descriptor.initializer();
+    }
+
+    return {
+        enumerable: true,
+        configurable: true,
+        get() {
+            return value;
+        },
+        set(val) {
+            value = val;
+            this.forceUpdate();
+        }
+    };
 }
--- tests/integration/App.jsx
+++ tests/integration/App.jsx
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import {track} from './../../src';
 
 export default class App extends Component {
+
+    @track count = 0;
+
+    increment() {
+        this.count++;
+        this.count++;
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                {this.count}
+                <button onClick={() => this.increment()}>Increment</button>
+            </div>
         );
     }
 }
